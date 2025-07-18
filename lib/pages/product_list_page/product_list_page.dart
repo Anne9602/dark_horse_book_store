0dc9441553c2628cbe1f24304275074b2d33dd04
Anne <hyeyeong0203@naver.com>
2025-07-18 19:36:05 +0900

    feature: 상품리스트목록 구현

--- lib/pages/product_list_page/product_list_page.dart
+++ lib/pages/product_list_page/product_list_page.dart
@@ -1,3 +1,4 @@
+import 'package:dark_horse_book_store/common_widgets/product_item.dart';
 import 'package:dark_horse_book_store/common_widgets/widgets.dart';
 import 'package:flutter/material.dart';
 
@@ -11,6 +12,21 @@ class ProductListPage extends StatefulWidget {
 class _ProductListPageState extends State<ProductListPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: BuildAppbar(title: '🐴 다크호스 서점'));
+    return Scaffold(
+      appBar: BuildAppbar(title: '🐴 다크호스 서점'),
+      body: Padding(
+        padding: const EdgeInsets.all(10),
+        child: ListView.separated(
+          separatorBuilder: (context, index) {
+            return SizedBox(height: 10);
+          },
+          itemCount: 10,
+          itemBuilder: (context, index) {
+            //상품 목록 리스트
+            return ProductItem(bookTitle: '삐에로는 우릴보며 웃지', price: 15000);
+          },
+        ),
+      ),
+    );
   }
 }
