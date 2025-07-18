5c0982d6a3e60241761303e17c28373b1256be54
Anne <hyeyeong0203@naver.com>
2025-07-19 00:19:05 +0900

    feature: 플로팅버튼구현

--- lib/pages/product_list_page/product_list_page.dart
+++ lib/pages/product_list_page/product_list_page.dart
@@ -1,6 +1,7 @@
 import 'package:dark_horse_book_store/common_widgets/product_item.dart';
 import 'package:dark_horse_book_store/common_widgets/widgets.dart';
 import 'package:dark_horse_book_store/model/book.dart';
+import 'package:dark_horse_book_store/pages/product_add_page/product_add_page.dart';
 import 'package:flutter/material.dart';
 
 class ProductListPage extends StatefulWidget {
@@ -22,6 +23,7 @@ class _ProductListPageState extends State<ProductListPage> {
       body: Padding(
         padding: const EdgeInsets.all(10),
         child:
+            //리스트에 아무것도 없을 경우 상품없음으로 표시
             books.isEmpty
                 ? Container(
                   height: 140,
@@ -42,10 +44,34 @@ class _ProductListPageState extends State<ProductListPage> {
                   itemCount: books.length,
                   itemBuilder: (context, index) {
                     //상품 목록 리스트
-                    return ProductItem(book: books[index]);
+                    return GestureDetector(
+                      child: ProductItem(book: books[index]),
+                      onTap: () {
+                        //상품상페이지로이동
+                      },
+                    );
                   },
                 ),
       ),
+      //상품 등록 페이지로 이동
+      floatingActionButton: floatingButton(context),
+    );
+  }
+
+  //플로팅 버튼
+  FloatingActionButton floatingButton(BuildContext context) {
+    return FloatingActionButton(
+      onPressed: () {
+        //상품 등록 페이지로 이동
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => ProductAddPage()),
+        );
+      },
+      backgroundColor: Colors.brown,
+      foregroundColor: Colors.white,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(100)),
+      child: Icon(Icons.add, size: 30),
     );
   }
 }
