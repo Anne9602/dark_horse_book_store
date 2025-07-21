8953e497382dd9c25064aedf70f40d2d498725da
Anne <hyeyeong0203@naver.com>
2025-07-21 14:08:34 +0900

    feat: 상품설명 입력란 maxline수정

--- lib/pages/product_add_page/product_add_page.dart
+++ lib/pages/product_add_page/product_add_page.dart
@@ -13,92 +13,95 @@ class ProductAddPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: BuildAppbar(title: "상품 등록"),
-      body: Padding(
-        padding: const EdgeInsets.all(10),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            //상품 이미지 등록칸
-            Container(
-              width: double.infinity,
-              height: 350,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(8),
-                color: Colors.brown[100],
-              ),
-              child: Center(
-                child: Text(
-                  '이미지 선택',
-                  style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.brown,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(10),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              //상품 이미지 등록칸
+              Container(
+                width: double.infinity,
+                height: 350,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(8),
+                  color: Colors.brown[100],
+                ),
+                child: Center(
+                  child: Text(
+                    '이미지 선택',
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.brown,
+                    ),
                   ),
                 ),
               ),
-            ),
 
-            SizedBox(height: 20),
-            //1. 상품 이름 입력란
-            Text(
-              '상품 이름',
-              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-            ),
-            SizedBox(height: 10),
-            _textformField(
-              _textEditingTitleController,
-              '상품이름을 입력하세요',
-              TextInputAction.next,
-            ),
-            SizedBox(height: 20),
-            //2. 성품 가격 입련란
-            Text(
-              '상품 가격',
-              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-            ),
-            SizedBox(height: 10),
-            _textformField(
-              _textEditingPriceController,
-              '상품 가격을 입력해주세요',
-              TextInputAction.next,
-            ),
-            SizedBox(height: 20),
-            //3. 상품 설명 입력란
-            Text(
-              '상품 설명',
-              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-            ),
-            SizedBox(height: 10),
-            _textformField(
-              _textEditingContentController,
-              '상품 설명을 입력해주세요',
-              TextInputAction.done,
-            ),
+              SizedBox(height: 20),
+              //1. 상품 이름 입력란
+              Text(
+                '상품 이름',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: 10),
+              _textformField(
+                _textEditingTitleController,
+                '상품이름을 입력하세요',
+                TextInputAction.next,
+              ),
+              SizedBox(height: 20),
+              //2. 성품 가격 입련란
+              Text(
+                '상품 가격',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: 10),
+              _textformField(
+                _textEditingPriceController,
+                '상품 가격을 입력해주세요',
+                TextInputAction.next,
+              ),
+              SizedBox(height: 20),
+              //3. 상품 설명 입력란
+              Text(
+                '상품 설명',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: 10),
+              _textformField(
+                _textEditingContentController,
+                '상품 설명을 입력해주세요',
+                TextInputAction.done,
+                5, // 최대 줄 수 설정
+              ),
 
-            //등록버튼
-            SizedBox(height: 20),
-            SizedBox(
-              height: 50,
-              width: double.infinity,
+              //등록버튼
+              SizedBox(height: 20),
+              SizedBox(
+                height: 50,
+                width: double.infinity,
 
-              child: ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.brown,
-                  foregroundColor: Colors.white,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10),
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.brown,
+                    foregroundColor: Colors.white,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                  onPressed: () {
+                    //등록 버튼 누르기
+                  },
+                  child: Text(
+                    '등록하기',
+                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
                   ),
                 ),
-                onPressed: () {
-                  //등록 버튼 누르기
-                },
-                child: Text(
-                  '등록하기',
-                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
@@ -108,10 +111,12 @@ class ProductAddPage extends StatelessWidget {
   TextFormField _textformField(
     controller,
     String hintText,
-    TextInputAction inputAction,
-  ) {
+    TextInputAction inputAction, [
+    int? maxLine, //옵션파라미터
+  ]) {
     return TextFormField(
       controller: controller,
+      maxLines: maxLine ?? 1, //기본 한줄세팅
       decoration: InputDecoration(
         hintText: hintText,
         enabledBorder: OutlineInputBorder(
